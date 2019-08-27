3f7ef859310c90664c3507d6bc2668fb6ccb6744
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-27 11:12:52 -0400

    Add common to correct compiler error in PTSalesPerson class

--- Employees/PTSalesPerson.cs
+++ Employees/PTSalesPerson.cs
@@ -8,7 +8,7 @@ namespace Employees
 {
     sealed class PTSalesPerson : SalesPerson
     {
-        public PTSalesPerson(string fullName int age, int empID,
+        public PTSalesPerson(string fullName, int age, int empID,
             float currPay, string ssn, int numbOfSales)
             : base(fullName, age, empID, currPay, ssn, numbOfSales)
         {
