180b7021f1a90996758f8f2452b503fa02f9fb00
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-27 14:50:13 -0400

    Add casting examples.

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -8,6 +8,29 @@ namespace Employees
 {
     class Program
     {
+        static void CastingExamples()
+        {
+            // A Manager "is-a" System.Object, so we can store a Manager reference in an object variable just fine.
+            object frank = new Manager("Frank Zappa", 9, 3000, 40000, "111-11-1111", 5);
+            GivePromotion((Manager)frank);
+
+            // A Manager "is-an" Employee too.
+            Employee moonUnit = new Manager("MoonUnit Zappa", 2, 3001, 20000, "101-11-1321", 1);
+            GivePromotion(moonUnit);
+
+            // A PTSalesPerson "is-a" SalesPerson.
+            SalesPerson jill = new PTSalesPerson("Jill", 834, 3002, 100000, "111-12-1119", 90);
+            GivePromotion(jill);
+        }
+
+        static void GivePromotion(Employee emp)
+        {
+            // Increase pay...
+            // Give new parking space in company garage...
+
+            Console.WriteLine("{0} was promoted!", emp.Name);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("***** The Employee Class Hierarchy *****\n");
