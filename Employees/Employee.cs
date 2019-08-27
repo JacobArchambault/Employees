72831bc5c8566231960f445c2aa49a8942de898f
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-27 07:48:15 -0400

    Add Employee subclasses.

---
+++ Employees/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    partial class Employee
+    {
+        // Methods.
+        public void GiveBonus(float amount)
+        {Pay += amount;}
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("ID: {0}", ID);
+            Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Pay: {0}", Pay);
+        }
+
+        // Properties
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                else
+                    empName = value;
+            }
+        }
+
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+
+        public int Age
+        {
+            get => empAge;
+            set => empAge = value;
+        }
+
+
+    }
+}
