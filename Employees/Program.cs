08bc7d0d10ea31c86e77c4af75f81cff3f7d3bc2
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-27 11:40:25 -0400

    GiveBonus to chucky and fran.

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -18,6 +18,13 @@ namespace Employees
 
             Manager chucky = new Manager("Chucky", 50, 92, 100000, "333-23-2322", 9000);
             double cost = chucky.GetBenefitCost();
+            chucky.GiveBonus(300);
+            chucky.DisplayStats();
+            Console.WriteLine();
+
+            SalesPerson fran = new SalesPerson("Fran", 43, 93, 3000, "932-32-3232", 31);
+            fran.GiveBonus(200);
+            fran.DisplayStats();
 
             Console.ReadLine();
         }
